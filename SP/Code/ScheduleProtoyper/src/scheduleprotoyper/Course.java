8943c865d6a13d4891470e7d2cb8bdbe64fb8218
hhale <hhale@eastsideprep.org>
2017-05-01 20:08:28 -0700

    more

--- SP/Code/ScheduleProtoyper/src/scheduleprotoyper/Course.java
+++ SP/Code/ScheduleProtoyper/src/scheduleprotoyper/Course.java
@@ -1,62 +1,38 @@
 package scheduleprotoyper;
 
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-
+import static scheduleprotoyper.SchedulePrototyper.MasterContext;
 /**
  *
  * @author hhale
  */
-public class Course extends Rectangle {
+public class Course {
 
+    //These are the rectangles that represent courses you have in your schedule.
     private Color color;
     private String name;
     private String subject;
     private String teacher;
     private Character period;
     private int grade;
+    private int x;
+    private int y;
     private int UIx;
     private int UIy;
-    //UIx and UIy refer to a UI coordinate system to be made by Justice.
-    boolean grayedOut;
-
-    public void erase() {
-        this.setFill(Color.WHITE);
-    }
-
-    public void draw() {
-        //In the future the coordinates should be adjusted here
-        //to draw the courses at their correct sizes instead of 200*200.
-        this.setX(this.UIx * 200);
-        this.setY(this.UIy * 200);
-        if (this.grayedOut == true) {
-            this.setFill(this.color.desaturate());
-        } else {
-            this.setFill(this.color);
-        }
-    }
-
-    public void move(int x, int y) {
-        this.erase();
-        this.UIx += x;
-        this.UIy += y;
-        this.grayedOut = (this.UIx != 0);
-        //grays out colors that are out of the main row
-        this.draw();
-    }
+    //UIx and UIy refer to a future UI coordinate system to be made by Justice.
+    private boolean grayedOut;
 
     Course(String input) {
-        //period, grades allowed, class name, teacher, credit type *break*
-        //parse the strings here
+        //String format: period, grades allowed, class name, teacher, credit type *break*
+        //parse those strings here later
 
-        //placeholders
-        super(0, 0, Color.WHITE);
+        //placeholders below
         this.grade = 9;
         this.name = "Biology";
         this.teacher = "Waltzer";
         this.period = 'A';
-        this.setY((int) this.period);
-        this.setX(0);
+        this.UIy = (int) this.period - 1;
+        this.UIx = 0;
 
         //the colors are the same as on the current pdf schedules
         //sorted by period
@@ -86,6 +62,34 @@ public class Course extends Rectangle {
                 this.color = Color.PINK;
                 break;
         }
+
+        this.draw();
+    }
+
+    public void erase() {
+        this.color = Color.WHITE;
+    }
+
+    public void draw() {
+        //In the future the coordinates should be adjusted here
+        //to draw the courses at their correct sizes instead of 200*200.
+        this.x = this.UIx * 200;
+        this.y = this.UIy * 150;
+        if (this.grayedOut == true) {
+            MasterContext.setFill(this.color.desaturate());
+        } else {
+            MasterContext.setFill(this.color);
+        }
+        MasterContext.fillRect(this.x, this.y, 200, 150);
+    }
+
+    public void move(int x, int y) {
+        this.erase();
+        this.UIx += x;
+        this.UIy += y;
+        this.grayedOut = (this.UIx != 0);
+        //grays out colors that are out of the main row
+        this.draw();
     }
 
     public Color getColor() {
@@ -120,5 +124,13 @@ public class Course extends Rectangle {
     public int getUIy() {
         //the y coordinate relative to other courses
         return this.UIy;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
     }
 }
--- SP/Code/ScheduleProtoyper/src/scheduleprotoyper/SchedulePrototyper.java
+++ SP/Code/ScheduleProtoyper/src/scheduleprotoyper/SchedulePrototyper.java
@@ -2,24 +2,27 @@ package scheduleprotoyper;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 /**
  *
  * @author hhale 
  */
 public class SchedulePrototyper extends Application {
     
+    public static GraphicsContext MasterContext;
+    
     @Override
     public void start(Stage primaryStage) {
         
         StackPane root = new StackPane();
+        Canvas canvas = new Canvas(400, 400);
+        root.getChildren().add(canvas);
+        MasterContext = canvas.getGraphicsContext2D();
         Course testCourse = new Course("placeholder string");
-        root.getChildren().add(testCourse);
-        
         Scene scene = new Scene(root, 300, 250);
-        
         primaryStage.setTitle("Schedule Prototyper");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -27,7 +30,6 @@ public class SchedulePrototyper extends Application {
         
         
     }
-
     /**
      * @param args the command line arguments
      */
