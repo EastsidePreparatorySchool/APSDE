12ab3c29a2571a44959d9ac779796dc27bfc6344
MillanShenoy <mshenoy_2@eastsideprep.org>
2017-05-16 08:09:57 -0700

    Experimental UI
    Unnecessary UI, don't look here for now

---
+++ VEP/UITest/src/uitest/UITest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uitest;
+
+/**
+ *
+ * @author mshenoy_2
+ */
+public class UITest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LogIn login = new LogIn();
+        login.setVisible(true);
+        
+    }
+    
+}
